cc904d7046d6e7a0a54a84c916eda28b5410f4b4
talesprates <talesprates@hotmail.com>
2017-08-28 22:17:06 -0300

    todo: vehicle wheels

--- integrations/chatbot/commands/vehicle-tracking.js
+++ integrations/chatbot/commands/vehicle-tracking.js
@@ -10,54 +10,83 @@ module.exports = {
 function vehicleTrack(message, callback, wheels) {
   const trackedPlayers = variables.TRACKED_PLAYERS;
   Promise.all(trackedPlayers.map(personaId => getVehicle(personaId, wheels)))
-    .then(generateServerMessage)
+    .then(playersVehicles => generateServerMessage(playersVehicles, wheels))
     .then(serverMessage => callback(serverMessage.join('\n')))
     .catch(error => callback(`error retrieving vehicle info (${error})`));
 }
 
 function getVehicle(personaId, wheels) {
+  return typeof (personaId) === 'string' ? getSingleVehicle(personaId, wheels) : getMultipleVehicles(personaId, wheels);
+}
+
+function getMultipleVehicles(personaList, wheels) {
+  return new Promise((resolve, reject) =>
+    Promise.all(personaList.map(personaId => getSingleVehicle(personaId, wheels)))
+      .then((vehicleList) => {
+        const reducedVehicleList = vehicleList.reduce((v1, v2) => {
+          v2.name = v1.name;
+          v2.vehicle = v2.vehicle.map((v, index) => {
+            v.kills += v1.vehicle[index].kills;
+            return v;
+          });
+          return v2;
+        });
+        resolve(reducedVehicleList);
+      })
+      .catch(reject)
+  );
+}
+
+function getSingleVehicle(personaId, wheels) {
   return new Promise((resolve, reject) => {
     persona.getPersona(personaId)
       .then((playerPersona) => {
-        let wheelsStats;
         if (wheels) {
           vehicleStats.getMainVehicleStats(personaId)
-          .then((wheelsData) => {
-            const found = wheelsData.mainVehicleStats.some((vehicle) => {
-              if (vehicle.slug.toUpperCase().indexOf(wheels.toUpperCase()) !== -1) {
-                wheelsStats = vehicle;
-                return true;
-              }
-              return false;
-            });
-
-            if (!found) {
-              reject('invalid vehicle');
-            } else {
-              resolve(
-                {
-                  name: playerPersona.personaName,
-                  vehicle: wheelsStats
-                });
-            }
-          })
-          .catch(reject);
+            .then(vehiclesData => findVehicle(vehiclesData.mainVehicleStats, wheels))
+            .then(vehicle => resolve({ name: playerPersona.personaName, vehicle }))
+            .catch(reject);
         } else {
           vehicleStats.getMainVehicleStats(personaId)
-            .then(weaponsData => resolve(
-              {
-                name: playerPersona.personaName,
-                vehicle: weaponsData.mainVehicleStats.sort((v1, v2) => v2.kills - v1.kills)[0]
-              }))
+            .then(vehiclesData => resolve(
+              { name: playerPersona.personaName, vehicle: vehiclesData.mainVehicleStats }))
             .catch(reject);
         }
       });
   });
 }
 
-function generateServerMessage(playersVehicles) {
+function findVehicle(vehicles, wheels) {
+  return new Promise((resolve, reject) => {
+    let wheelsStats;
+    const found = vehicles.some((vehicle) => {
+      if (vehicle.slug.toUpperCase().indexOf(wheels.toUpperCase()) !== -1) {
+        wheelsStats = vehicle;
+        return true;
+      }
+      return false;
+    });
+
+    if (found) {
+      resolve(wheelsStats);
+    } else {
+      reject('invalid vehicle');
+    }
+  });
+}
+
+function generateServerMessage(playersVehicles, wheels) {
+  console.log(playersVehicles);
   return new Promise((resolve) => {
     const serverMessage = [];
+
+    if (!wheels) {
+      playersVehicles.forEach((player) => {
+        player.vehicle.sort((v1, v2) => v2.kills - v1.kills);
+        player.vehicle = player.vehicle[0];
+      });
+    }
+
     const sortedPlayersVehicles = playersVehicles
       .sort((playerA, playerB) => playerB.vehicle.kills - playerA.vehicle.kills);
 
